04cf72cf3bfa214eebe4ad67a0b15ade91778ade
Andrea <andrea.rinaldi91@gmail.com>
2020-11-11 09:30:18 +0100

    Add RxLibraries framework

---
+++ RxLibraries/RxLibraries.h
@@ -0,0 +1,18 @@
+//
+//  RxLibraries.h
+//  RxLibraries
+//
+//  Created by Andrea Rinaldi on 11/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxLibraries.
+FOUNDATION_EXPORT double RxLibrariesVersionNumber;
+
+//! Project version string for RxLibraries.
+FOUNDATION_EXPORT const unsigned char RxLibrariesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxLibraries/PublicHeader.h>
+
+
