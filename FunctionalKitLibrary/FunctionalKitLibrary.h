dc81ee781b1097c5d9f0fc68f578d211c3995a32
Andrea <andrea.rinaldi91@gmail.com>
2020-11-12 10:33:00 +0100

    Create FunctionalKit module

---
+++ FunctionalKitLibrary/FunctionalKitLibrary.h
@@ -0,0 +1,18 @@
+//
+//  FunctionalKitLibrary.h
+//  FunctionalKitLibrary
+//
+//  Created by Andrea Rinaldi on 12/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FunctionalKitLibrary.
+FOUNDATION_EXPORT double FunctionalKitLibraryVersionNumber;
+
+//! Project version string for FunctionalKitLibrary.
+FOUNDATION_EXPORT const unsigned char FunctionalKitLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalKitLibrary/PublicHeader.h>
+
+
